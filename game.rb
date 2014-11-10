25d3f5cae9366e8c720c7f74a0ee8df58981b55a
Rich Shea <richcshea@gmail.com>
2014-11-10 12:52:34 -0700

    Add code and test for guess check

--- game.rb
+++ game.rb
@@ -7,4 +7,12 @@ class Game
   def quit
     "\nI'm sorry to see you go. Come back soon!"
   end
+
+  def guess_prompt
+    "Please enter a guess"
+  end
+
+  def retrieve_guess
+    'rrby'
+  end
 end
--- game_test.rb
+++ game_test.rb
@@ -17,4 +17,16 @@ class GameTest < Minitest::Test
     assert_equal "\nI'm sorry to see you go. Come back soon!", game.quit
   end
 
+  def test_it_asks_player_to_enter_a_guess
+    game = Game.new
+
+    assert_equal "Please enter a guess", game.guess_prompt
+  end
+
+  def test_it_retrieves_player_guess
+    game = Game.new
+
+    assert_equal 'rrby', game.retrieve_guess
+  end
+
 end
