0b4b4a73b03e570dbc03fd6f95a9b4ae4bea02ca
Rich Shea <richcshea@gmail.com>
2014-11-12 21:30:28 -0700

    added starts with counter at zero test

--- test/game_test.rb
+++ test/game_test.rb
@@ -4,6 +4,12 @@ require 'minitest/pride'
 require_relative '../lib/game'
 
 class GameTest < Minitest::Test
-  
+
+  def test_game_starts_with_counter_at_zero
+    game = Game.new
+    result = game.counter
+
+    assert_equal 0, result
+  end
 
 end
