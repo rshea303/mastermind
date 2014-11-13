283b6b9fa057d91a6bc131254e0dd2ce85570120
Rich Shea <richcshea@gmail.com>
2014-11-13 11:07:36 -0700

    guess and guesses

--- lib/game.rb
+++ lib/game.rb
@@ -36,7 +36,7 @@ class Game
             end
           end
         end
-        puts codemaker_feedback(player_guess)
+        codemaker_feedback(player_guess)
     end
     finish_time = Time.now
     winner(start_time, finish_time)
@@ -67,11 +67,11 @@ class Game
     location_match = guess_checker.location_match(guess, solution)
     puts "'#{solution.upcase}' = solution" #this is just for guess verification during manual testing
     puts "'#{guess.upcase}' has #{total_correct} of the correct elements with #{location_match} in the correct positions."
-      unless counter == 1
-        puts "You've taken #{counter} guesses"
-      else
-        puts "You've taken #{counter} guess"
-      end
+    if counter == 1
+      puts "You've taken #{counter} guess"
+    else
+      puts "You've taken #{counter} guesses"
+    end
   end
 
   #private
